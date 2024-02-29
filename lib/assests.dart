5580ef6d0317233642b1a141f074a2bf248add67
ganesg <sampathiganesh0106@gmail.com>
2024-02-29 16:40:54 +0530

    new project

---
+++ lib/assests.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class AssestsPage extends StatelessWidget {
+  const AssestsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Assests Page'),
+      ),
+      body: ListView(
+        children: const [
+          ListTile(
+            title: Text('assests 1'),
+          ),
+          Divider(
+            thickness: 2.0,
+          ),
+          ListTile(
+            title: Text('assests 2'),
+          ),
+          Divider(
+            thickness: 2.0,
+          )
+          // Add more ListTiles as needed
+        ],
+      ),
+    );
+  }
+}
+
+// void main() {
+//   runApp(MaterialApp(
+//     home: AssestsPage(),
+//   ));
+// }
---
+++ lib/tests.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class TestsPage extends StatelessWidget {
+  const TestsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tests Page'),
+      ),
+      body: ListView(
+        children: const [
+          ListTile(
+            title: Text('test 1'),
+          ),
+          Divider(
+            thickness: 2.0,
+          ),
+          ListTile(
+            title: Text('test 2'),
+          ),
+          Divider(
+            thickness: 2.0,
+          )
+          // Add more ListTiles as needed
+        ],
+      ),
+    );
+  }
+}
+
+// void main() {
+//   runApp(MaterialApp(
+//     home: AssestsPage(),
+//   ));
+// }
