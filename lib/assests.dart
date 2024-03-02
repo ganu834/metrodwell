4403fe97d089624f2ca5cfacf6338c3dfc3c93a4
ganesg <sampathiganesh0106@gmail.com>
2024-03-02 11:41:53 +0530

    changes

--- lib/assests.dart
+++ lib/assests.dart
@@ -1,29 +1,68 @@
 import 'package:flutter/material.dart';
 
+// class AssestsPage extends StatelessWidget {
+//   const AssestsPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Assests Page'),
+//       ),
+
+//       body: ListView(
+//         children: const [
+//           ListTile(
+//             title: Text('assests 3'),
+//           ),
+//           Divider(
+//             thickness: 2.0,
+//           ),
+//           ListTile(
+//             title: Text('assests 2'),
+//           ),
+//           Divider(
+//             thickness: 2.0,
+//           )
+//           // Add more ListTiles as needed
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 class AssestsPage extends StatelessWidget {
   const AssestsPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Assests Page'),
+        title: const Text('Assets Page'),
       ),
-      body: ListView(
-        children: const [
-          ListTile(
-            title: Text('assests 3'),
-          ),
-          Divider(
+      body: Column(
+        children: [
+          const Divider(
             thickness: 2.0,
           ),
-          ListTile(
-            title: Text('assests 2'),
+          Expanded(
+            child: ListView(
+              children: const [
+                ListTile(
+                  title: Text('Assets 3'),
+                ),
+                Divider(
+                  thickness: 2.0,
+                ),
+                ListTile(
+                  title: Text('Assets 2'),
+                ),
+                Divider(
+                  thickness: 2.0,
+                ),
+                // Add more ListTiles as needed
+              ],
+            ),
           ),
-          Divider(
-            thickness: 2.0,
-          )
-          // Add more ListTiles as needed
         ],
       ),
     );
